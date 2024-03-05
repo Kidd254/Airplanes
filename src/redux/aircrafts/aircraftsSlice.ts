99e0e21b2784663764f3144fa55ab0a46dcca3e7
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-03-05 14:29:53 +0300

    Remove unneccesary import

--- src/redux/aircrafts/aircraftsSlice.ts
+++ src/redux/aircrafts/aircraftsSlice.ts
@@ -1,22 +1,21 @@
 // aircraftsSlice.ts
 import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { SerializedError } from '@reduxjs/toolkit';
 
 interface Aircraft {
-    manufacturer: string;
-    model: string;
-    engine_type: string;
-    min_speed: number;
-    max_speed: number;
-    min_range: number;
-    max_range: number;
-    min_length: number;
-    max_length: number;
-    min_height: number;
-    max_height: number;
-    min_wingspan: number;
-    max_wingspan: number;
+  manufacturer: string;
+  model: string;
+  engine_type: string;
+  min_speed: number;
+  max_speed: number;
+  min_range: number;
+  max_range: number;
+  min_length: number;
+  max_length: number;
+  min_height: number;
+  max_height: number;
+  min_wingspan: number;
+  max_wingspan: number;
 }
 
 interface AircraftsState {
@@ -28,41 +27,56 @@ interface AircraftsState {
 const initialState: AircraftsState = {
   data: [],
   loading: false,
-  error: null,
+  error: null
 };
 
-export const fetchAircrafts = createAsyncThunk('aircrafts/fetchAircrafts', async (api_key: string, thunkAPI) => {
-  try {
-    const response = await axios.get('https://api.api-ninjas.com/v1/aircraft', {
-      headers: {
-        'X-Api-Key': api_key,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
+export const fetchAircrafts = createAsyncThunk(
+  'aircrafts/fetchAircrafts',
+  async (
+    {
+      api_key,
+      manufacturer,
+      model
+    }: { api_key: string; manufacturer: string; model: string },
+    thunkAPI
+  ) => {
+    try {
+      const apiUrl = `https://api.api-ninjas.com/v1/aircraft?manufacturer=${manufacturer}&model=${model}`;
+      const response = await axios.get(apiUrl, {
+        headers: {
+          'X-Api-Key': api_key
+        }
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   }
-});
+);
 
 const aircraftsSlice = createSlice({
-    name: 'aircrafts',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchAircrafts.pending, (state) => {
-                state.loading = true;
-                state.error = null;
-            })
-            .addCase(fetchAircrafts.fulfilled, (state, action: PayloadAction<Aircraft[]>) => {
-                state.loading = false;
-                state.data = action.payload;
-            })
-            .addCase(fetchAircrafts.rejected, (state, action: PayloadAction<unknown, string, { arg: string; requestId: string; requestStatus: "rejected"; aborted: boolean; condition: boolean; } & ({ rejectedWithValue: true; } | ({ rejectedWithValue: false; } & {})), SerializedError>) => {
-                state.loading = false;
-                state.error = action.payload as string;
-            });
-    },
+  name: 'aircrafts',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAircrafts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchAircrafts.fulfilled,
+        (state, action: PayloadAction<Aircraft[]>) => {
+          state.loading = false;
+          state.data = action.payload;
+        }
+      )
+      .addCase(fetchAircrafts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      }
+      );
+  }
 });
 
 export default aircraftsSlice.reducer;
