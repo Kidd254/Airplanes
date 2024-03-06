1521bd819a42aefe73e6d08aaf80191d4faa3e17
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-03-06 16:38:49 +0300

    Add general styling to all components

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
+import './index.css';
 
 const root = createRoot(document.getElementById('root')!); // Use non-null assertion here
 
